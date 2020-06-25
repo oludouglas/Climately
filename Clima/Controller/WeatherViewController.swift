7408a501c73a79b1b2c31a7b1688028a73c5ed00
Olu Douglas <olupotd3@gmail.com>
2020-06-26 01:28:48 +0300

    fixed location manager retrigger

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -7,18 +7,102 @@
 //
 
 import UIKit
+import CoreLocation
 
 class WeatherViewController: UIViewController {
-
+    
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
+    
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var cityTextField: UITextField!
+    
+    let locationManager = CLLocationManager()
+    var weatherMaster: WeatherMaster?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        locationManager.delegate = self
+        //request for permission to access location
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
+        
+        cityTextField.delegate = self
+        weatherMaster = WeatherMaster()
+        weatherMaster?.delegate = self
     }
-
-
+    
+    
+    @IBAction func locationPressed(_ sender: UIButton) {
+        locationManager.requestLocation()
+    }
+    
 }
 
+//MARK: -LocationManager extension
+
+extension WeatherViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            locationManager.stopUpdatingLocation()
+            let lat = location.coordinate.latitude 
+            let long = location.coordinate.longitude
+            weatherMaster?.fetchWeather(lat: lat, lon: long)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+}
+
+//MARK: -WeatherDelegate
+
+extension WeatherViewController: WeatherDelegate{
+    
+    func didUpdateWeather(_ weatherMaster: WeatherMaster, weather: WeatherModel) {
+        print(weather.cityName)
+        DispatchQueue.main.async {
+            self.temperatureLabel.text = weather.tempString
+            self.cityLabel.text = weather.cityName
+            self.conditionImageView.image = UIImage(systemName: weather.condition)
+        }
+    }
+    
+    func didFailWithError(_ weatherMaster: WeatherMaster, error: Error) {
+        print(error)
+    }
+    
+}
+
+//MARK: -UITextFieldDelegate
+
+extension WeatherViewController: UITextFieldDelegate{
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        if let city = cityTextField.text {
+            weatherMaster?.fetchWeather(city: city)
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        cityTextField.endEditing(true)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        }
+        textField.placeholder = ""
+        return false
+    }
+    
+    @IBAction func searchCityPressed(_ sender: UIButton) {
+        cityTextField.endEditing(true)
+    }
+    
+}
---
+++ Clima/Model/WeatherMaster.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  Clima
+//
+//  Created by Angela Yu on 01/09/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var cityTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        cityTextField.delegate = self
+    }
+
+    @IBAction func searchCityPressed(_ sender: UIButton) {
+        cityTextField.endEditing(true)
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        print(textField.text!)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        cityTextField.endEditing(true)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        }
+        textField.placeholder = ""
+        return false
+    }
+    
+}
+
